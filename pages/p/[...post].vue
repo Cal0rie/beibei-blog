78ae210f3eb5f712a521fb06280731ddfd8332ac
Cal0rie <zmxianyu@163.com>
2024-04-30 09:16:38 +0000

    fixed: 评论配置

--- pages/p/[...post].vue
+++ pages/p/[...post].vue
@@ -14,8 +14,8 @@ const path = post.join('/')
         <doc-render :article="doc" />
         <doc-toc :toc="doc.body.toc" />
         <Giscus
-          repo="chansee97/nuxt-blog" repo-id="R_kgDOJZM7Qg" category="comments"
-          category-id="DIC_kwDOJZM7Qs4CW1GZ" mapping="title" term="Welcome to my blog!" reactions-enabled="1"
+          repo="Cal0rie/beibei-blog" repo-id="R_kgDOKsIe1Q" category="comments"
+          category-id="DIC_kwDOKsIe1c4CfCuP" mapping="title" term="Welcome to Beibei blog!" reactions-enabled="1"
           emit-metadata="1" input-position="top" theme="light_tritanopia" lang="zh-CN" loading="lazy"
         />
       </template>
